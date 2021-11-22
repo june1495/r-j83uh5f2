68b305fbbf19258fa346b0a2d5ea8797b6ed09c5
Franco <francomelgar4@gmail.com>
2021-11-22 12:52:32 -0500

    commited

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      contador: 0,
+      value: "",
+    };
+    this.OnWriteHandle = this.OnWriteHandle.bind(this);
+  }
+  OnWriteHandle(event) {
+    this.setState({
+      contador: event.target.value.length,
+      value: event.target.value,
+    });
+  }
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea
+          rows="3"
+          onChange={this.OnWriteHandle}
+          value={this.state.value}
+        ></textarea>
+        <div className="counter">{this.state.contador}</div>
       </div>
     );
   }
 }
-
 export default App;
